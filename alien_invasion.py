d5ae6826976427ae6fbd4c4d6734b27f1755b73c
ttamanna <t.tamanna312@gmail.com>
2023-02-23 13:29:48 -0600

    Updated alien_invasion.py and ship.py for movement to right

--- alien_invasion.py
+++ alien_invasion.py
@@ -29,6 +29,7 @@ class AlienInvasion:
         while True:
           
           self._check_events()
+          self.ship.update()
           self._update_screen()
 
     def _check_events(self):
@@ -41,8 +42,13 @@ class AlienInvasion:
             
            elif event.type == pygame.KEYDOWN:
                if event.key == pygame.K_RIGHT:
-                   # Move the ship to the right.
-                   self.ship.rect.x += 1
+                   self.ship.moving_right = True
+               elif event.type == pygame.KEYUP:
+                    if event.key == pygame.K_RIGHT:
+                        self.ship.moving_right = False
+        
+                    self.ship.rect.x += 1
+       
 
     def _update_screen(self):
         # Update images on the screen, and flip to the new screen.

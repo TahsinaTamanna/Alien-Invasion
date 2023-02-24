ade3850692189c515508997e92ece2bb6c3c609f
ttamanna <t.tamanna312@gmail.com>
2023-02-24 17:45:19 -0600

    Refactored _create_fleet()

--- alien_invasion.py
+++ alien_invasion.py
@@ -121,11 +121,15 @@ class AlienInvasion:
         
         # Create the first row of aliens.
         for alien_number in range(number_aliens_x):
-            # Create an alien and place it in the row.
+            self._create_alien(alien_number)
+
+    def _create_alien(self, alien_number):
+        # Create an alien and place it in the row.
             alien = Alien(self)
+            alien_width = alien.rect.width
             alien.x = alien_width + 2 * alien_width * alien_number
             alien.rect.x = alien.x
-            self.aliens.add(alien)
+            self.aliens.add(alien)       
     
 if __name__ == '__main__':
    # Make a game instance, and run the game.

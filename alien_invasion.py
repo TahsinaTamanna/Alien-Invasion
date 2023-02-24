62771c6a2ed5079158e33e98539cce181f784f58
ttamanna <t.tamanna312@gmail.com>
2023-02-24 16:47:48 -0600

    Updated code to run the game in full screen

--- alien_invasion.py
+++ alien_invasion.py
@@ -14,7 +14,12 @@ class AlienInvasion:
 
         # Settings
         self.settings = Settings()
-        self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
+
+        self.screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+        self.settings.screen_width = self.screen.get_rect().width
+        self.settings.screen_height = self.screen.get_rect().height
+
+        #self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
 
         # Set the background color.
@@ -55,6 +60,9 @@ class AlienInvasion:
         
         elif event.key == pygame.K_LEFT:
             self.ship.moving_left = True
+        
+        elif event.key == pygame.K_q: # Q to quit
+            sys.exit()
 
 
     def _check_keyup_events(self, event):

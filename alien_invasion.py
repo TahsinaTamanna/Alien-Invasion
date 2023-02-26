9f9665536028d08d81890b461eff71352b9f7d0d
ttamanna <t.tamanna312@gmail.com>
2023-02-26 01:40:24 -0600

    Checking aliens at the bottom of the screen

--- alien_invasion.py
+++ alien_invasion.py
@@ -166,6 +166,8 @@ class AlienInvasion:
         # Look for alien-ship collisions.
         if pygame.sprite.spritecollideany(self.ship, self.aliens):
             self._ship_hit()
+        # Look for aliens hitting the bottom of the screen.
+        self._check_aliens_bottom()
 
 
     def _check_bullet_alien_collisions(self):
@@ -180,7 +182,7 @@ class AlienInvasion:
 
 
     def _ship_hit(self):
-        # Respond to the ship being hit by an alien."""
+        # Respond to the ship being hit by an alien.
         # Decrement ships_left.
         self.stats.ships_left -= 1    
         # Get rid of any remaining aliens and bullets.
@@ -191,6 +193,15 @@ class AlienInvasion:
         self.ship.center_ship()
         # Pause.
         sleep(0.5)
+
+    def _check_aliens_bottom(self):
+        # Check if any aliens have reached the bottom of the screen.
+        screen_rect = self.screen.get_rect()
+        for alien in self.aliens.sprites():
+            if alien.rect.bottom >= screen_rect.bottom:
+                # Treat this the same as if the ship got hit.
+                self._ship_hit()
+                break
 
 
 if __name__ == '__main__':

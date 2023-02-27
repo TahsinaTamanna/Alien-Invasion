594c6a4a4defb580957f1a1174853f4dfb97dfd2
ttamanna <t.tamanna312@gmail.com>
2023-02-27 17:09:09 -0600

    Updated code to modify speed settings

--- alien_invasion.py
+++ alien_invasion.py
@@ -78,7 +78,6 @@ class AlienInvasion:
         # Start a new game when the player clicks Play."""
         button_clicked = self.play_button.rect.collidepoint(mouse_pos)
         if button_clicked and not self.stats.game_active:
-        #if self.play_button.rect.collidepoint(mouse_pos):
             # Reset the game statistics.
             self.stats.reset_stats()
             self.stats.game_active = True 
@@ -89,6 +88,9 @@ class AlienInvasion:
             # Create a new fleet and center the ship.
             self._create_fleet()
             self.ship.center_ship()   
+            
+            # Hide the mouse cursor.
+            pygame.mouse.set_visible(False)
     
     def _check_keydown_events(self, event):
         # Respond to keypresses.
@@ -224,6 +226,8 @@ class AlienInvasion:
             sleep(0.5)
         else:
             self.stats.game_active = False
+        
+        pygame.mouse.set_visible(True)
 
     def _check_aliens_bottom(self):
         # Check if any aliens have reached the bottom of the screen.
--- settings.py
+++ settings.py
@@ -3,7 +3,7 @@ class Settings:
     
     def __init__(self):
         
-    # Initialize the game's settings
+        # Initialize the game's static settings
         # Screen settings
         self.screen_width = 1200
         self.screen_height = 800
@@ -24,5 +24,15 @@ class Settings:
         # Alien settings
         self.alien_speed = 1.0
         self.fleet_drop_speed = 50
+
+        # How quickly the game speeds up
+        self.speedup_scale = 1.1
+        self.initialize_dynamic_settings()
+    
+    def initialize_dynamic_settings(self):
+        # "Initialize settings that change throughout the game.
+        self.ship_speed = 1.5
+        self.bullet_speed = 3.0
+        self.alien_speed = 1.0
         # fleet_direction of 1 represents right; -1 represents left.
         self.fleet_direction = 1

e1122aa0852969a83ae0f68c0308aeb655dfbddb
ttamanna <t.tamanna312@gmail.com>
2023-02-27 17:44:15 -0600

    Updated codes to increase speed and point values

--- alien_invasion.py
+++ alien_invasion.py
@@ -85,6 +85,7 @@ class AlienInvasion:
             # Reset the game statistics.
             self.stats.reset_stats()
             self.stats.game_active = True 
+            self.sb.prep_score()
             # Get rid of any remaining aliens and bullets.
             self.aliens.empty()
             self.bullets.empty()
@@ -140,8 +141,9 @@ class AlienInvasion:
         collisions = pygame.sprite.groupcollide(self.bullets, self.aliens, True, True)
 
         if collisions:
-            self.stats.score += self.settings.alien_points
-            self.sb.prep_score()
+            for aliens in collisions.values():
+                self.stats.score += self.settings.alien_points * len(aliens)
+                self.sb.prep_score()
         
         if not self.aliens:
             # Destroy existing bullets and create new fleet.
--- scoreboard.py
+++ scoreboard.py
@@ -19,7 +19,9 @@ class Scoreboard:
 
     def prep_score(self):
         # Turn the score into a rendered image.
-        score_str = str(self.stats.score)
+        #score_str = str(self.stats.score)
+        rounded_score = round(self.stats.score, -1)
+        score_str = "{:,}".format(rounded_score)
         self.score_image = self.font.render(score_str, True,
         self.text_color, self.settings.bg_color)
         
@@ -31,4 +33,3 @@ class Scoreboard:
     def show_score(self):
         # Draw score to the screen.
         self.screen.blit(self.score_image, self.score_rect)
-        

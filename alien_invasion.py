23c5e8e95602101aea3e6e77964b81a62810aa6c
ttamanna <t.tamanna312@gmail.com>
2023-02-27 17:27:02 -0600

    Modified alien_invasion.py to show scores

--- alien_invasion.py
+++ alien_invasion.py
@@ -9,6 +9,7 @@ from bullet import Bullet
 from alien import Alien
 from game_stats import GameStats
 from button import Button
+from scoreboard import Scoreboard
 
 class AlienInvasion:
     # Overall class to manage game assets and behavior
@@ -31,8 +32,9 @@ class AlienInvasion:
         # Set the background color.
         self.bg_color = (230, 230, 230)
         
-        # Create an instance to store game statistics.
+        # Create an instance to store game statistics, and create a scoreboard.
         self.stats = GameStats(self)   
+        self.sb = Scoreboard(self)
 
         self.ship = Ship(self)
         self.bullets = pygame.sprite.Group()
@@ -154,6 +156,9 @@ class AlienInvasion:
             bullet.draw_bullet()
 
         self.aliens.draw(self.screen)
+
+        # Draw the score information.
+        self.sb.show_score()
 
         # Draw the play button if the game is inactive.
         if not self.stats.game_active:

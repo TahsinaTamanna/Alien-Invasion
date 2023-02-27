68ae245b8980298b72db221eb84796371ea3e049
ttamanna <t.tamanna312@gmail.com>
2023-02-27 17:30:29 -0600

    Updating scores

--- settings.py
+++ settings.py
@@ -30,12 +30,14 @@ class Settings:
         self.initialize_dynamic_settings()
     
     def initialize_dynamic_settings(self):
-        # "Initialize settings that change throughout the game.
+        # Initialize settings that change throughout the game.
         self.ship_speed = 1.5
         self.bullet_speed = 3.0
         self.alien_speed = 1.0
         # fleet_direction of 1 represents right; -1 represents left.
         self.fleet_direction = 1
+        # Scoring
+        self.alien_points = 50
 
     def increase_speed(self):
         # Increase speed settings.

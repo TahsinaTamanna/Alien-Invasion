5ce68d622f5f61c83e1e6e0ce2f1f4a452e2736e
ttamanna <t.tamanna312@gmail.com>
2023-02-28 10:08:45 -0600

    Updated bulled width, drop speed and speddup scale

--- settings.py
+++ settings.py
@@ -15,17 +15,17 @@ class Settings:
 
         # Bullet settings
         self.bullet_speed = 1.5
-        self.bullet_width = 3
+        self.bullet_width = 3 #
         self.bullet_height = 15
         self.bullet_color = (60, 60, 60)
         self.bullets_allowed = 3
 
         # Alien settings
         self.alien_speed = 1.0
-        self.fleet_drop_speed = 1.5
+        self.fleet_drop_speed = 1.5 #
 
         # How quickly the game speeds up
-        self.speedup_scale = 1.1
+        self.speedup_scale = 1.1 #
         # How quickly the alien point values increase
         self.score_scale = 1.5
         self.initialize_dynamic_settings()
